9f2907c161dc4dec909b2ebce34bbbf06df9a45e
keigo LaTe4 EMURA <late4@134.co.jp>
2016-05-04 15:26:46 +0000

    Revert "Revert "Making s3-upload.""
    This reverts commit 5c81b8d1df8faa13fa1247f7726d445ae1931724.

---
+++ public/work/s3-upload.js
@@ -0,0 +1,79 @@
+
+var fileNum = 1; // XXX
+
+/* // XXX
+
+function loadTemplate {
+     var req = new XMLHttpRequest();
+     req.onload = function() {
+          if (req.readyState == 4) {
+
+          }
+     }
+     reqBase.open("GET",baseTemplateUrl,true);
+     reqBase.send();
+}
+
+*/
+
+window.onload = function() {
+     listFile();
+}
+
+function selectFile(num) {
+     var selector = document.getElementById("f-file"+num);
+     selector.click();
+}
+
+function updateFile(num) {
+     var display = document.getElementById("disp-file"+num);
+     var selector = document.getElementById("f-file"+num);
+     if (selector.value) {
+          var filename = selector.value.split("\\");
+          var basename = filename[filename.length - 1];
+          display.innerHTML = basename;
+
+          s3Upload(selector.files[0]);
+     }
+}
+
+function s3Upload(file) {
+     AWS.config.update(
+          {
+               accessKeyId: 'AKIAIPCJ5MFLIBTD7UVQ',
+               secretAccessKey: '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', // XXX: hardcoded
+               region: 'ap-northeast-1'
+
+          }
+     );
+     AWS.config.region = 'ap-northeast-1';
+
+     var bucket = new AWS.S3(
+          {
+               params: {
+                    Bucket: 'bem134-portal-files'
+               }
+          }
+     );
+     var params = { Key: 'test-upload.txt', ContentType: file.type, Body: file };
+     bucket.upload(params, function (err, data) {
+          console.log(err ? "error" : "saved")
+     });
+}
+
+function listFile() {
+
+     // XXX
+
+}
+
+function addFile() {
+     var template = document.getElementById('template-addfile').innerHTML;
+     var table = document.getElementById('table-files').innerHTML;
+     fileNum++;
+
+     var newRow = template.replace(/__FILENUMBER__/g, fileNum.toString());
+     console.log(table);
+
+     document.getElementById('table-files').innerHTML += newRow;
+}
